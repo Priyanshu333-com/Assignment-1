cc3aa1f80583d2b8ae917db26a8f57b05e12417e
Priyanshu333-com <priyanshu34920@gmail.com>
2025-12-15 20:05:25 +0530

    Assignment 1

---
+++ Task1.py
@@ -0,0 +1,14 @@
+a = float(input("Enter 1st number: "))
+b = float(input("Enter 2nd number: "))
+
+# operations
+
+add = a + b
+sub = a - b
+mul = a * b
+div = a / b
+
+print("Addition : ", add)
+print("Subtraction : ", sub)
+print("Multiplication: ", mul)
+print("Division: ", div)
